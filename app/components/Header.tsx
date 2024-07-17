dc6c0ba04e53766c51753c5892d2b7846e091698
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-07-17 15:10:57 +0200

    update header design

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,11 +6,14 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.imageContainer}>
-        <Image src="/images/headPic2.png" alt="Profile photo" width={150} height={150} className={styles.photo} />
+        <Image src="/images/headPic2.png" alt="Profile photo" layout="responsive"  width={150} height={150} className={styles.photo} />
       </div>
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Emilien Hainaux</h1>
-        <p className={styles.subtitle}>Concepteur développeur web & mobile</p>
+        <div className={styles.subtitleContainer}>
+          <span className={styles.subtitle}>Concepteur développeur</span>
+          <span className={styles.subtitle}>web & mobile</span>
+        </div>
       </div>
     </header>
   );
