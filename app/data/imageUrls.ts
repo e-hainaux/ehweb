c0402ad0e29874825412f2b7ffef521d92d11f5f
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-09-27 11:09:23 +0200

    update refacto imageUrls in data

---
+++ app/data/imageUrls.ts
@@ -0,0 +1,12 @@
+const imageUrls = [
+  "/images/portfolio.png",
+  "/images/headPic.png",
+  "/images/QJO.png",
+  "/images/HelenePetSitting.png",
+  "/images/SYA.png",
+  "/images/DressCode.png",
+  "/images/GaminRetro.png",
+  "/images/FibroQuoi.png",
+];
+
+export default imageUrls;
--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,105 +1,25 @@
 "use client";
 
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ContactPopover from "../components/ContactPopover";
-import { FaAt } from "react-icons/fa";
 import LoadingProgress from "../components/LoadingProgress";
-
 import StackBlock from "../components/StackBlock";
 import InfoBlock from "../components/InfoBlock";
 import RealisationsBlock from "../components/RealisationsBlock";
+import useImagePreloader from "../hooks/useImagePreloader";
+import imageUrls from "../data/imageUrls";
 
 const Home: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [showPopover, setShowPopover] = useState(false);
-  const [spin, setSpin] = useState(true);
-  const [returnToInitial, setReturnToInitial] = useState(false);
-  const [loadingProgress, setLoadingProgress] = useState(0);
-  const [isTransitioning, setIsTransitioning] = useState(false);
-  const [isCarouselReady, setIsCarouselReady] = useState(false);
-
-  const content = (
-    <div>
-      <p>
-        Retrouvez-moi sur{" "}
-        <a
-          className={styles.a}
-          href="https://www.linkedin.com/in/e-hainaux"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-      </p>
-    </div>
-  );
-
-  const handleVisibleChange = (visible: boolean) => {
-    setShowPopover(visible);
-    if (visible) {
-      setSpin(false);
-    } else {
-      setReturnToInitial(true);
-      setTimeout(() => {
-        setSpin(true);
-        setReturnToInitial(false);
-      }, 200);
-    }
-  };
+  const { isLoading, loadingProgress, isTransitioning, isCarouselReady } =
+    useImagePreloader(imageUrls);
 
   useEffect(() => {
     return () => {
       document.body.style.overflow = "unset";
     };
-  }, []);
-
-  useEffect(() => {
-    const imageUrls = [
-      "/images/portfolio.png",
-      "/images/headPic.png",
-      "/images/QJO.png",
-      "/images/HelenePetSitting.png",
-      "/images/SYA.png",
-      "/images/DressCode.png",
-      "/images/GaminRetro.png",
-      "/images/FibroQuoi.png",
-    ];
-
-    const preloadImages = async () => {
-      const totalImages = imageUrls.length;
-      let loadedImages = 0;
-
-      const imagePromises = imageUrls.map(
-        (src) =>
-          new Promise<void>((resolve, reject) => {
-            const img = document.createElement("img");
-            img.src = src;
-            img.onload = () => {
-              loadedImages++;
-              setLoadingProgress((loadedImages / totalImages) * 100);
-              resolve();
-            };
-            img.onerror = reject;
-          })
-      );
-
-      try {
-        await Promise.all(imagePromises);
-        setIsTransitioning(true);
-        setIsCarouselReady(true);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Failed to load images:", error);
-        setIsTransitioning(true);
-        setIsCarouselReady(false);
-        setIsLoading(false);
-      }
-    };
-
-    preloadImages();
   }, []);
 
   if (isLoading) {
